01fd5b352ce71992889c4acd52756b3391995fd5
guillaume60240 <77913074+guillaume60240@users.noreply.github.com>
2021-05-14 16:38:38 +0200

    modiffication style

--- Views/functionView/remplirSuggestion1View.php
+++ Views/functionView/remplirSuggestion1View.php
@@ -1,7 +1,7 @@
 
 
 <div class="vid3">
-    <h5><?= $videoTitle?></h5>
+    <h5 class="suggestionTitre"><?= $videoTitle?></h5>
     <div class="suggestionBefore">
         <iframe <?=$linkS?>></iframe>
     </div>
