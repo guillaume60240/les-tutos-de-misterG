c34ab53f9b8abe5f1740eb7c362f968abfed12de
guillaume60240 <77913074+guillaume60240@users.noreply.github.com>
2021-06-03 18:47:43 +0200

    modif css

--- Views/templates/base.php
+++ Views/templates/base.php
@@ -12,6 +12,8 @@
     <meta property="og:title" content="Les tutos de mister G">
     <link rel="stylesheet" href="../style/styleHeaderFooter.css">
     <link rel="stylesheet" href="../style/defaultStyle.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <title><?= $title ?></title>
 </head>
 
